e90370ed429b145fb6629e843167ded39e9fd000
gon2santos <necchigonzalo@gmail.com>
2022-11-08 23:37:12 -0300

    fix(client): add buffer override and fix package.json scripts

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,12 +1,23 @@
-module.exports = function override (config, env) {
-    console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
-        "stream": require.resolve("stream-browserify"),
-        "util": require.resolve("util/"),
-        "crypto": require.resolve("crypto-browserify"),
-        "buffer": require.resolve("buffer"),
-        "Buffer": require.resolve("buffer"),
+const webpack = require("webpack")
+
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+        crypto: require.resolve("crypto-browserify"),
+        util: require.resolve("util/"),
+        'process/browser': require.resolve('process/browser')
     }
+    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+    config.plugins = [
+        ...config.plugins,
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]
+
     return config
 }
