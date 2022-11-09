952f312b5a7e3d2bf1e9bad3417d6160971a8bcf
gon2santos <necchigonzalo@gmail.com>
2022-11-08 22:42:55 -0300

    fix(client): override webpack config

---
+++ client/config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = function override (config, env) {
+    console.log('override')
+    let loaders = config.resolve
+    loaders.fallback = {
+        "stream": require.resolve("stream-browserify"),
+        "util": require.resolve("util/"),
+        "crypto": require.resolve("crypto-browserify"),
+        "buffer": require.resolve("buffer")
+    }
+    return config
+}
